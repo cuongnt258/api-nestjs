4f8550545a3096dd0a112f2b4b5fad896592f0bd
Cuong Nguyen <cuongnt258@gmail.com>
2023-01-04 22:43:37 +0700

    fix mongodb connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { ArticleModule } from './article/article.module';
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true }),
     UserModule,
     AuthModule,
     ArticleModule,
